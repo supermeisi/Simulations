ec09c909112c0a97585f1e05cff0f3848372bbd8
supermeisi <mustafa_schmidt@gmx.de>
2025-05-08 06:37:30 +0000

    Fixing issues with energy loss calculations

--- energy.py
+++ energy.py
@@ -33,28 +33,30 @@ energy_loss = np.zeros((len(thicknesses), len(momenta)))
 
 n_glass = f.rindex(500)
 
+m_mu = f.mmu * 1e3
+
 # Main simulation loop
 for i, thickness in enumerate(thicknesses):
   steps = int(thickness / step_cm)
   for j, p0 in enumerate(momenta):
     print(f"Thickness: {thickness} cm, Momentum: {p0} MeV/c")
-    E = np.sqrt(p0**2 + f.mmu**2)
+    E = np.sqrt(p0**2 + m_mu**2)
     E_initial = E
     p = p0
     beta = p / E
-    gamma = E / f.mmu
+    gamma = E / m_mu
     angles = []
     initial_angle = f.cherenkov(beta, n_glass)
     for _ in range(steps):
       angles.append(f.cherenkov(beta, n_glass))
       dE = f.bethe_bloch(f.mmu, beta, gamma) * step_cm
       E -= dE
-      if E <= f.mmu:
-        E = f.mmu
+      if m_mu >= E:
+        E = m_mu
         break
-      p = np.sqrt(E**2 - f.mmu**2)
+      p = np.sqrt(E**2 - m_mu**2)
       beta = p / E
-      gamma = E / f.mmu
+      gamma = E / m_mu
     # RMS of Cherenkov angle change (mrad)
     if len(angles) >= 2:
       angles = np.array(angles)
@@ -64,26 +66,38 @@ for i, thickness in enumerate(thicknesses):
     rms_angle_change[i, j] = rms * 1e3
     energy_loss[i, j] = E_initial - E  # total energy loss in MeV
 
-
-# Plotting side-by-side heatmaps
-fig, axs = plt.subplots(1, 2, figsize=(14, 6))
-
 extent = [momenta_GeV[0], momenta_GeV[-1], thicknesses[0], thicknesses[-1]]
 
 # Cherenkov RMS
-im1 = axs[0].imshow(rms_angle_change, aspect='auto', origin='lower', extent=extent, cmap='gist_rainbow_r', norm=colors.LogNorm())
-axs[0].set_title('Cherenkov Angle Smearing')
-axs[0].set_xlabel('$p$ [GeV/c]')
-axs[0].set_ylabel('$x$ [cm]')
-cbar1 = fig.colorbar(im1, ax=axs[0])
+fig = plt.figure(figsize=(5, 4))
+im1 = plt.imshow(rms_angle_change,
+                 aspect='auto',
+                 origin='lower',
+                 extent=extent,
+                 cmap='gist_rainbow_r',
+                 norm=colors.LogNorm())
+plt.title('Cherenkov Angle Smearing')
+plt.xlabel('$p$ [GeV/c]')
+plt.ylabel('$x$ [cm]')
+cbar1 = fig.colorbar(im1)
 cbar1.set_label('$\\sigma$ [mrad]')
 
+plt.tight_layout()
+plt.savefig('cherenkov_energy.png', dpi=600)
+plt.savefig('cherenkov_energy.pdf')
+plt.close()
+
 # Energy Loss
-im2 = axs[1].imshow(energy_loss, aspect='auto', origin='lower', extent=extent, cmap='gist_rainbow_r')
-axs[1].set_title('Energy Loss')
-axs[1].set_xlabel('$p$ [GeV/c]')
-axs[1].set_ylabel('$x$ [cm]')
-cbar2 = fig.colorbar(im2, ax=axs[1])
+fig = plt.figure(figsize=(5, 4))
+im2 = plt.imshow(energy_loss,
+                 aspect='auto',
+                 origin='lower',
+                 extent=extent,
+                 cmap='gist_rainbow_r')
+plt.title('Energy Loss')
+plt.xlabel('$p$ [GeV/c]')
+plt.ylabel('$x$ [cm]')
+cbar2 = fig.colorbar(im2)
 cbar2.set_label('$\\Delta E$ [MeV]')
 
 plt.tight_layout()
